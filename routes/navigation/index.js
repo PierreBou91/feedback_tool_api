b2e13e8b96a2998f5870444e4c4e21d636efa4e4
PierreBou <pierre.bouriat@outlook.fr>
2022-08-21 18:53:32 +0200

    fix: cors on nav

--- routes/navigation/index.js
+++ routes/navigation/index.js
@@ -1,10 +1,15 @@
-'use strict'
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
+"use strict";
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const cors = require("@fastify/cors");
 
 module.exports = async function (fastify, opts) {
-    const nav = await prisma.navigation.findMany()
-  fastify.get('/', async function (request, reply) {
-    return nav
-  })
-}
+  await fastify.register(cors, {
+    origin: "*",
+    methods: ["GET"],
+  });
+  const nav = await prisma.navigation.findMany();
+  fastify.get("/", async function (request, reply) {
+    return nav;
+  });
+};
