612d3f0f705272df90b8d0d602a2fa36a616d86d
PierreBou <pierre.bouriat@outlook.fr>
2022-08-22 22:07:16 +0200

    git: nobody gets it

--- routes/navigation/index.js
+++ routes/navigation/index.js
@@ -3,14 +3,8 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = async function (fastify, opts) {
-
   fastify.get("/", async function (request, reply) {
     const nav = await prisma.navigation.findMany();
-<<<<<<< Updated upstream
-    return nav
-=======
-    reply.send(nav);
-    return;
->>>>>>> Stashed changes
+    return nav;
   });
 };
