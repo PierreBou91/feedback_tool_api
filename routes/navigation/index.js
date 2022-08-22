fdabc11493b24f8399538cf9fd02a8a1c52e001e
PierreBou <pierre.bouriat@outlook.fr>
2022-08-22 22:05:14 +0200

    Auto stash before merge of "main" and "origin/main"

--- routes/navigation/index.js
+++ routes/navigation/index.js
@@ -6,6 +6,11 @@ module.exports = async function (fastify, opts) {
 
   fastify.get("/", async function (request, reply) {
     const nav = await prisma.navigation.findMany();
+<<<<<<< Updated upstream
     return nav
+=======
+    reply.send(nav);
+    return;
+>>>>>>> Stashed changes
   });
 };
