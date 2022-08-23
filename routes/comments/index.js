3dc07e3092b916c80bf56c60083cd605e6e2010e
PierreBou <pierre.bouriat@outlook.fr>
2022-08-24 00:01:50 +0200

    feat: fetch comment by feedbackId

--- routes/comments/index.js
+++ routes/comments/index.js
@@ -1,10 +1,24 @@
-'use strict'
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
+"use strict";
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 module.exports = async function (fastify, opts) {
-    const comments = await prisma.comment.findMany()
-  fastify.get('/', async function (request, reply) {
-    return comments
-  })
-}
+  fastify.get("/", async function (request, reply) {
+    if (request.query["feedbackId"]) {
+      console.log("been here");
+      console.log(request.query["feedbackId"]);
+      const comments = await prisma.comment.findMany({
+        where: {
+          feedbackId: request.query["feedbackId"],
+        },
+        include: {
+          author: true,
+        },
+      });
+      return comments;
+    }
+
+    const comments = await prisma.comment.findMany();
+    return comments;
+  });
+};
--- routes/feedbacks/index.js
+++ routes/feedbacks/index.js
@@ -1,32 +1,27 @@
-'use strict'
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+"use strict";
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 module.exports = async function (fastify, opts) {
-  // await fastify.register(cors, {
-  //   origin: "*",
-  //   methods: ["GET"],
-  // });
-  fastify.get('/', async function (request, reply) {
+  fastify.get("/", async function (request, reply) {
     const feedbacks = await prisma.feedback.findMany({
       include: {
         company: true,
-      }
-    })
-    return feedbacks
-  })
+      },
+    });
+    return feedbacks;
+  });
 
-  fastify.get('/:id', async function (request, reply) {
-    const { id } = request.params
+  fastify.get("/:id", async function (request, reply) {
+    const { id } = request.params;
     const feedback = await prisma.feedback.findUnique({
       where: {
-        id: id
+        id: id,
       },
       include: {
         company: true,
-        comments: true,
-      }
-    })
-    return feedback
-  })
-}
+      },
+    });
+    return feedback;
+  });
+};
